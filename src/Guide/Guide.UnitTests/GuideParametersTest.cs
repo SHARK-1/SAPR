69460907d4010f26a486f7a7443a741b3a112afc
Alexandr Matinin <sashasss95@gmail.com>
2021-12-16 15:16:42 +0700

    Тесты параметров

--- src/Guide/Guide.UnitTests/GuideParametersTest.cs
+++ src/Guide/Guide.UnitTests/GuideParametersTest.cs
@@ -12,21 +12,13 @@ namespace Guide.UnitTests
 
     public class GuideParametersTest
     {
-        public void NegativeTest(ParametersEnum parametersEnum, double value)
-        {
-            //Arrange
-            var expectedValue = value;
-            var parameter = new GuideParameters();
-
-            //Assert
-            Assert.Throws <TargetInvocationException > (() =>
-            {
-                //Act
-                var propertyInfo = typeof(GuideParameters).
-                        GetProperty(parametersEnum.ToString());
-                propertyInfo.SetValue(parameter, value);
-            });
-        }
+        [TestCase(ParametersEnum.GuideLength,60, TestName = "Длинна направляющей, позитив")]
+        [TestCase(ParametersEnum.GuideWidth, 11, TestName = "Ширина направляющей, позитив")]
+        [TestCase(ParametersEnum.GuideDepth, 6, TestName = "Толщина направляющей, позитив")]
+        [TestCase(ParametersEnum.AttachmentStrokeLength, 16, TestName = "Длинна хода крепления, позитив")]
+        [TestCase(ParametersEnum.AttachmentStrokeWidth, 4, TestName = "Ширина хода крепления, позитив")]
+        [TestCase(ParametersEnum.HoleDiameter, 3, TestName = "Диаметр отверстия, позитив")]
+        [TestCase(ParametersEnum.GuideAngle, 100, TestName = "Угол наклона направляющей, позитив")]
         public void PositiveTest(ParametersEnum parametersEnum, double value)
         {
             //Arrange
@@ -41,115 +33,75 @@ namespace Guide.UnitTests
             //Assert
             Assert.AreEqual(expectedValue, propertyInfo.GetValue(parameter));
         }
-        [TestCase(TestName = "Длинна направляющей, позитив")]
-        public void TestSetGuideLength_CurrectValue_ResultCorrectSet()
+        [TestCase(ParametersEnum.GuideLength, 30, TestName = "Длинна направляющей, меньше положенного, исключение")]
+        [TestCase(ParametersEnum.GuideLength, 160, TestName = "Длинна направляющей, больше положенного, исключение")]
+        [TestCase(ParametersEnum.GuideWidth, 5, TestName = "Ширина направляющей, меньше положенного, исключение")]
+        [TestCase(ParametersEnum.GuideWidth, 60, TestName = "Ширина направляющей, больше положенного, исключение")]
+        [TestCase(ParametersEnum.GuideDepth, 1, TestName = "Толщина направляющей, меньше положенного, исключение")]
+        [TestCase(ParametersEnum.GuideDepth, 60, TestName = "Толщина направляющей, больше положенного, исключение")]
+        [TestCase(ParametersEnum.AttachmentStrokeLength, 1, TestName = "Длинна хода крепления, меньше положенного, исключение")]
+        [TestCase(ParametersEnum.AttachmentStrokeLength, 100, TestName = "Длинна хода крепления, больше положенного, исключение")]
+        [TestCase(ParametersEnum.AttachmentStrokeWidth, 1, TestName = "Ширина хода крепления, меньше положенного, исключение")]
+        [TestCase(ParametersEnum.AttachmentStrokeWidth, 60, TestName = "Ширина хода крепления, больше положенного, исключение")]
+        [TestCase(ParametersEnum.HoleDiameter, 1, TestName = "Диаметр отверстия, меньше положенного, исключение")]
+        [TestCase(ParametersEnum.HoleDiameter, 60, TestName = "Диаметр отверстия, больше положенного, исключение")]
+        [TestCase(ParametersEnum.GuideAngle, 60, TestName = "Угол наклона направляющей, меньше положенного, исключение")]
+        [TestCase(ParametersEnum.GuideAngle, 300, TestName = "Угол наклона направляющей, больше положенного, исключение")]
+        public void NegativeTest(ParametersEnum parametersEnum, double value)
         {
-            PositiveTest(ParametersEnum.GuideLength, 60);
-        }
-        [TestCase(TestName = "Длинна направляющей меньше положенного")]
-        public void TestSetGuideLength_IncurrectValueLess50_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.GuideLength, 40);
-        }
-        [TestCase(TestName = "Длинна направляющей больше положенного")]
-        public void TestSetGuideLength_IncurrectValueMore150_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.GuideLength, 1);
-        }
-        [TestCase(TestName = "Ширину направляющей, позитив")]
-        public void TestSetGuideWidth_CurrectValue_ResultCorrectSet()
-        {
-            PositiveTest(ParametersEnum.GuideWidth, 15);
-        }
-        [TestCase(TestName = "Ширину направляющей меньше положенного")]
-        public void TestSetGuideWidth_IncurrectValueLess10_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.GuideWidth, 5);
-        }
-        [TestCase(TestName = "Ширину направляющей больше положенного")]
-        public void TestSetGuideWidth_IncurrectValueMore30_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.GuideWidth, 35);
-        }
+            //Arrange
+            var expectedValue = value;
+            var parameter = new GuideParameters();
 
-        [TestCase(TestName = "Толщину направляющей, позитив")]
-        public void TestSetGuideDepth_CurrectValue_ResultCorrectSet()
-        {
-            PositiveTest(ParametersEnum.GuideDepth, 15);
+            //Assert
+            Assert.Throws<TargetInvocationException>(() =>
+         {
+             //Act
+             var propertyInfo = typeof(GuideParameters).
+                  GetProperty(parametersEnum.ToString());
+             propertyInfo.SetValue(parameter, value);
+         });
         }
-        [TestCase(TestName = "Толщина направляющей меньше положенного")]
-        public void TestSetGuideDepth_IncurrectValueLess5_ArgumentException()
+        /// <summary>
+        /// Проверка максимальных минимальных значений параметра AttachmentStrokeWidth
+        /// </summary>
+        /// <param name="maxBorder">True-Максимальное, False- Минимальное</param>
+        [TestCase(true, TestName = "Максимальное значение параметра AttachmentStrokeWidth")]
+        [TestCase(false, TestName = "Минимальное значение параметра AttachmentStrokeWidth")]
+        public void CheckDiapozon_AttachmentStrokeWidth(bool isMaxBorder)
         {
-            NegativeTest(ParametersEnum.GuideDepth, 1);
-        }
-        [TestCase(TestName = "Толщина направляющей больше положенного")]
-        public void TestSetGuideDepth_IncurrectValueMore20_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.GuideDepth, 35);
-        }
+            double guideWidth = 20;
+            //Arrange
+            var expectedValue = isMaxBorder ? guideWidth * 0.5 : guideWidth * 0.3;
+            var parameter = new GuideParameters();
 
-        [TestCase(TestName = "Длинна хода крепления, позитив")]
-        public void TestSetAttachmentStrokeLength_CurrectValue_ResultCorrectSet()
-        {
-            PositiveTest(ParametersEnum.AttachmentStrokeLength, 15);
-        }
-        [TestCase(TestName = "Длинна хода крепления меньше положенного")]
-        public void TestSetAttachmentStrokeLength_IncurrectValueLess15_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.AttachmentStrokeLength, 1);
-        }
-        [TestCase(TestName = "Длинна хода крепления больше положенного")]
-        public void TestSetAttachmentStrokeLength_IncurrectValueMore90_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.AttachmentStrokeLength, 91);
-        }
+            //Act
+            parameter.GuideWidth = guideWidth;
 
-        [TestCase(TestName = "Ширина хода крепления, позитив")]
-        public void TestSetAttachmentStrokeWidth_CurrectValue_ResultCorrectSet()
-        {
-            PositiveTest(ParametersEnum.AttachmentStrokeWidth, 4);
+            //Assert
+            if (isMaxBorder)
+            {
+                Assert.AreEqual(expectedValue, parameter.RangeDictionary[ParametersEnum.AttachmentStrokeWidth].Max);
+            }
+            else
+            {
+                Assert.AreEqual(expectedValue, parameter.RangeDictionary[ParametersEnum.AttachmentStrokeWidth].Min);
+            }
         }
-        [TestCase(TestName = "Ширина хода крепления меньше положенного")]
-        public void TestSetAttachmentStrokeWidth_IncurrectValueLess3_ArgumentException()
+        [TestCase(TestName = "Минимальное значение параметра AttachmentStrokeLenght")]
+        public void CheckDiapozon_AttachmentStrokeLenght()
         {
-            NegativeTest(ParametersEnum.AttachmentStrokeWidth, 1);
-        }
-        [TestCase(TestName = "Ширина хода крепления больше положенного")]
-        public void TestSetAttachmentStrokeWidth_IncurrectValueMore5_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.AttachmentStrokeWidth, 21);
-        }
+            double attachmentStrokeWidth = 5;
+            //Arrange
+            var expectedValue = 5 * attachmentStrokeWidth;
+            var parameter = new GuideParameters();
 
-        [TestCase(TestName = "Диаметр отвестия, позитив")]
-        public void TestSetHoleDiameter_CurrectValue_ResultCorrectSet()
-        {
-            PositiveTest(ParametersEnum.HoleDiameter, 4);
-        }
-        [TestCase(TestName = "Диаметр отвестия меньше положенного")]
-        public void TestSetHoleDiameter_IncurrectValueLess2_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.HoleDiameter, 1);
-        }
-        [TestCase(TestName = "Диаметр отвестия больше положенного")]
-        public void TestSetHoleDiameter_IncurrectValueMore20_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.HoleDiameter, 21);
-        }
+            //Act
+            parameter.AttachmentStrokeLength = 90;
+            parameter.AttachmentStrokeWidth = attachmentStrokeWidth;
 
-        [TestCase(TestName = "Угол наклона, позитив")]
-        public void TestSetGuideAngle_CurrectValue_ResultCorrectSet()
-        {
-            PositiveTest(ParametersEnum.GuideAngle, 90);
-        }
-        [TestCase(TestName = "Угол наклона меньше положенного")]
-        public void TestSetGuideAngle_IncurrectValueLess65_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.GuideAngle, 12);
-        }
-        [TestCase(TestName = "Угол наклона больше положенного")]
-        public void TestSetGuideAngle_IncurrectValueMore270_ArgumentException()
-        {
-            NegativeTest(ParametersEnum.GuideAngle, 275);
+            //Assert
+            Assert.AreEqual(expectedValue, parameter.RangeDictionary[ParametersEnum.AttachmentStrokeLength].Min);
         }
     }
 }

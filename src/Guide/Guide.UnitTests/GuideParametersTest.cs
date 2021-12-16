1b32c4d7b458baea410fe2a841872e98087135e8
Alexandr Matinin <sashasss95@gmail.com>
2021-12-16 15:40:04 +0700

    Range Тесты

--- src/Guide/Guide.UnitTests/GuideParametersTest.cs
+++ src/Guide/Guide.UnitTests/GuideParametersTest.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Reflection;
+﻿using System.Reflection;
 using NUnit.Framework;
-using Microsoft.VisualBasic;
 
 namespace Guide.UnitTests
 {
---
+++ src/Guide/Guide.UnitTests/RangeTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace Guide.UnitTests
+{
+    class RangeTest
+    {
+        [TestCase(true, TestName = "Конструктор, максимальное значение")]
+        [TestCase(false, TestName = "Конструктор, минимальное значение")]
+        public void RangeConstructor_Test(bool isMaxBorder)
+        {
+            //Arrange
+            var expectedValue = isMaxBorder?20:12;
+
+            //Act
+            var range = new Range(12, 20);
+
+            //Assert
+            if (isMaxBorder)
+            {
+                Assert.AreEqual(expectedValue, range.Max);
+            }
+            else
+            {
+                Assert.AreEqual(expectedValue, range.Min);
+            }
+        }
+        [TestCase(true, TestName = "Максимальное значение")]
+        [TestCase(false, TestName = "Минимальное значение")]
+        public void RangeMinMax_Test(bool isMaxBorder)
+        {
+            //Arrange
+            var expectedValue = isMaxBorder ? 20 : 12;
+
+            //Act
+            var range = new Range(0, 100);
+            if (isMaxBorder)
+            {
+                range.Max = expectedValue;
+            }
+            else
+            {
+                range.Min = expectedValue;
+            }
+
+            //Assert
+            if (isMaxBorder)
+            {
+                Assert.AreEqual(expectedValue, range.Max);
+            }
+            else
+            {
+                Assert.AreEqual(expectedValue, range.Min);
+            }
+        }
+    }
+}
